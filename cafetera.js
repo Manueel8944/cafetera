87e469cf3126c173a3589d7fdc025b71fa4baaa3
Manuel Añel Garcia <manegar@aulaestudio.es>
2025-02-11 16:05:12 +0100

    ejercicio terminado

--- cafetera.js
+++ cafetera.js
@@ -20,27 +20,50 @@ class Cafetera {
 
     crearbebida(){
         this.leerdatos();
-        let nombre = prompt("Nombre de la bebida: ")
-        let agua = parseInt(prompt("Cantidad de agua (max 1000): "))
-        let leche = parseInt(prompt("Cantidad de leche (max 500): "))
-        let cafe = parseInt(prompt("Cantidad de cafe (max 300): "))
-        let azucar = parseInt(prompt("Cantidad de azucar (max 200): "))
 
-        if(agua > 1000){
-            agua = 1000
+        let bien = false
+        let nombre
+
+        while(!bien){
+            nombre = prompt("Nombre de la bebida: ").toLowerCase()
+            let encontrado = false
+            for(let i=0;i<this.maquinacafe[2].length;i++){
+                if (this.maquinacafe[2][i]["bebida"]["nombre"] == nombre || nombre == ""){
+                    encontrado = true
+                }
+            }
+
+            if (encontrado == true){
+                console.log("Error: Nombre invalido")
+            }
+            else{
+                bien = true
+            }
         }
-        if(leche > 500){
-            leche = 500
+
+        let agua = parseInt(prompt("Cantidad de agua (max 1000): "))
+        while (agua > 1000 || isNaN(agua)) {
+            console.log("Error: valor introducido invalido")
+            agua = parseInt(prompt("Cantidad de agua (max 1000): "))
         }
-        if(cafe > 300){
-            cafe = 300
+        let leche = parseInt(prompt("Cantidad de leche (max 500): "))
+        while (leche > 500 || isNaN(leche)) {
+            console.log("Error: valor introducido invalido")
+            leche = parseInt(prompt("Cantidad de leche (max 500): "))
         }
-        if(azucar > 200){
-            azucar = 200
-        }   
+        let cafe = parseInt(prompt("Cantidad de cafe (max 300): "))
+        while (cafe > 300 || isNaN(cafe)) {
+            console.log("Error: valor introducido invalido")
+            cafe = parseInt(prompt("Cantidad de cafe (max 300): "))
+        }
+        let azucar = parseInt(prompt("Cantidad de azucar (max 200): "))
+        while (azucar > 200 || isNaN(azucar)) {
+            console.log("Error: valor introducido invalido")
+            azucar = parseInt(prompt("Cantidad de azucar (max 200): "))
+        }
 
         let nuevabebida = {
-            nombre: {"agua": agua, "leche": leche, "cafe": cafe, "azucar": azucar} 
+            bebida: {nombre: nombre, "agua": agua, "leche": leche, "cafe": cafe, "azucar": azucar} 
         }
 
         this.maquinacafe[2].push(nuevabebida)
@@ -62,6 +85,42 @@ class Cafetera {
         console.log("Ingredientes recargados exitosamente")
     }
 
+    bebidasdisponibles(){
+        this.leerdatos();
+        for(let i=0;i<this.maquinacafe[2].length;i++){
+            if (this.maquinacafe[2][i]["bebida"]["agua"] <= this.maquinacafe[1]["agua"] && this.maquinacafe[2][i]["bebida"]["leche"] <= this.maquinacafe[1]["leche"] && this.maquinacafe[2][i]["bebida"]["cafe"] <= this.maquinacafe[1]["cafe"] && this.maquinacafe[2][i]["bebida"]["azucar"] <= this.maquinacafe[1]["azucar"]){
+                console.log(i,"-",this.maquinacafe[2][i]["bebida"]["nombre"])
+            }
+        }
+    }
+
+    pedirbebida(){
+        this.leerdatos();
+        for(let i=0;i<this.maquinacafe[2].length;i++){
+                console.log(i,"-",this.maquinacafe[2][i]["bebida"]["nombre"])
+        }
+        let bebida = parseInt(prompt("Elije una bebida de las que ves por pantalla (numero): "))
+        if (isNaN(bebida) || bebida > this.maquinacafe[2].length - 1 || bebida < 0) {
+            console.log("Numero fuera de rango")
+        }
+        else if (this.maquinacafe[2][bebida]["bebida"]["agua"] > this.maquinacafe[1]["agua"] || this.maquinacafe[2][bebida]["bebida"]["leche"] > this.maquinacafe[1]["leche"] || this.maquinacafe[2][bebida]["bebida"]["cafe"] > this.maquinacafe[1]["cafe"] || this.maquinacafe[2][bebida]["bebida"]["azucar"] > this.maquinacafe[1]["azucar"]){
+            console.log("Error: bebida no disponible, elija una de las indicadas")
+        }
+
+        else{
+            this.maquinacafe[1]["agua"] -= this.maquinacafe[2][bebida]["bebida"]["agua"]
+            this.maquinacafe[1]["leche"] -= this.maquinacafe[2][bebida]["bebida"]["leche"]
+            this.maquinacafe[1]["cafe"] -= this.maquinacafe[2][bebida]["bebida"]["cafe"]
+            this.maquinacafe[1]["azucar"] -= this.maquinacafe[2][bebida]["bebida"]["azucar"]
+            pause = prompt(("Sirviendo Bebida ("+this.maquinacafe[2][bebida]["bebida"]["nombre"]+")..."))
+            pause = prompt(("Sirviendo Bebida ("+this.maquinacafe[2][bebida]["bebida"]["nombre"]+")......"))
+            pause = prompt(("Sirviendo Bebida ("+this.maquinacafe[2][bebida]["bebida"]["nombre"]+")........."))
+            console.log("Bebida servida correctamente!!")
+
+        }
+
+        this.escribirdatos()
+    }
 }
 
 function menu () {
@@ -89,6 +148,7 @@ function menu () {
             case 1: {
                 console.clear()
                 console.log("=== Bebidas disponibles ===") 
+                maquina.bebidasdisponibles()
                 pause = prompt("Pulse Enter para continuar...")
                 break;
             }
@@ -120,6 +180,7 @@ function menu () {
             case 5: {
                 console.clear()
                 console.log("=== Pedir bebida ===") 
+                maquina.pedirbebida()
                 pause = prompt("Pulse Enter para continuar...")
                 break;
             }
